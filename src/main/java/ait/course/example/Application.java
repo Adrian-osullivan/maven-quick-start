46867b68f34a03c03f0a134cb56a1eff52c40f8f
Adrian O Sullivan <61162963+Adrian-osullivan@users.noreply.github.com>
2022-02-05 08:24:02 +0000

    maven change

--- src/main/java/ait/course/example/Application.java
+++ src/main/java/ait/course/example/Application.java
@@ -1,6 +1,15 @@
 package ait.course.example;
-
+import java.util.*;
 public class Application {
+	public void greet() {
+		ArrayList<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		for (String greeting : greetings) {
+			System.out.println("Greeting: " + greeting);
+		}
+
+	}
+	
 	public Application () {
 		System.out.println ("Inside Application");
 	}
