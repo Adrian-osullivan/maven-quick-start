35f39b6343bdb014a28792d6f07e514a2b7c63a2
Adrian O Sullivan <61162963+Adrian-osullivan@users.noreply.github.com>
2022-02-07 08:44:33 +0000

    changes

--- src/main/java/ait/course/example/Application.java
+++ src/main/java/ait/course/example/Application.java
@@ -1,6 +1,11 @@
 package ait.course.example;
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
+	public int countWords (String words) {
+		String[] separateWords = StringUtils.split(words,' ');
+		return (separateWords == null)? 0 : separateWords.length;
+	}
 	public void greet() {
 		ArrayList<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
@@ -17,6 +22,9 @@ public class Application {
 	public static void main(String[] args){
 		System.out.println("Starting Application");
 		Application app = new Application();
+		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.print("Word count: " + count);
 
 	}	
 }
