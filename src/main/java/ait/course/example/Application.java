e43ea602a9f85bb87f4c6c5cb25d754fdc5ffc65
Adrian O Sullivan <61162963+Adrian-osullivan@users.noreply.github.com>
2022-02-07 08:57:29 +0000

    Fix error

--- src/main/java/ait/course/example/Application.java
+++ src/main/java/ait/course/example/Application.java
@@ -1,30 +1,31 @@
 package ait.course.example;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
-public class Application {
-	public int countWords (String words) {
-		String[] separateWords = StringUtils.split(words,' ');
-		return (separateWords == null)? 0 : separateWords.length;
-	}
-	public void greet() {
-		ArrayList<String> greetings = new ArrayList<>();
-		greetings.add("Hello");
-		for (String greeting : greetings) {
-			System.out.println("Greeting: " + greeting);
-		}
 
+public class Application {
+
+	public int countWords(String words){
+		String[] seperateWords = StringUtils.split(words,' ');
+		return (seperateWords == null)? 0:seperateWords.length;
 	}
-	
-	public Application () {
-		System.out.println ("Inside Application");
+
+	public void greet(){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		for (String greeting : greetings){
+			System.out.println("Greeting: "+greeting);
+		}
+	}
+
+	public Application(){
+		System.out.println("Inside Application");
 	}
 
 	public static void main(String[] args){
 		System.out.println("Starting Application");
 		Application app = new Application();
 		app.greet();
-		int count = app.countWords("I have four words");
-		System.out.print("Word count: " + count);
+	}
 
-	}	
 }
